47b7eca745c3ca33aa371107d52866ec948391a2
SudarshanSowndiah <142959397+SudarshanSowndiah@users.noreply.github.com>
2023-08-22 23:37:47 -0400

    Update Live Web Scraper and Display.py

--- Live WebScraper and Display.py
+++ Live WebScraper and Display.py
@@ -3,17 +3,6 @@
 # Dec/2022
 
 
-# This program consists of two types of data scraped from two different websites.
-# The first of the two is data extracted from the movie review website the IMDB which displays the user with top 100 movies of all time.
-# The second is the website related to sports which shows the user with the points table of the current year's(2022-2023) Laliga football tournament.
-# The user can choose to view either of them from the main menu he is displayed at the start of the program.
-# Everytime the user wants to view the data the code will scrape the live data from the website and data is stored separately in the system.
-# All the data displayed is real and live from the website, be it the IMDb rating or the Laliga points table.
-# The visualization of these data follows the same method, whatever data is displayed in a graph is real and live from the website.
-# The user has multiple graphs to view from from both the websites, the user is given the option to choose from the menu where he/she is explained what each graph visualizes.
-# Thus making it simple for the user to better understand the data.  
-
-
 # Required Imports/libs/modules/.....
 import requests
 import pandas as pd
